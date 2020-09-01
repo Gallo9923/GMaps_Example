e229010245490efa67872e10a6a7060be9c4a1e7
Felipe Sanchez <vegafelipe015@gmail.com>
2020-08-31 23:08:12 -0500

    Ready

--- Gmaps/Model/DataManager.cs
+++ Gmaps/Model/DataManager.cs
@@ -4,16 +4,16 @@ using System.IO;
 
 namespace Gmaps.Model
 {
-    class DataManager
+    class DataManager //Clase encargada de leer el dataset
     {
-
+        //Deberan poner la ruta de su pc
         private const string PATH = "D:\\Documentos\\VisualStudioWorkSpace\\GmapsBeta\\Gmaps\\Data\\data.csv";
-        List<string> lista;
+        List<string> lista; //Aqui van los municipios
 
         public DataManager()
         { 
             
-            lista = new List<string>();
+            lista = new List<string>(); 
             readInfo();
 
         }
@@ -29,8 +29,8 @@ namespace Gmaps.Model
                 var line = reader.ReadLine();
                 var arreglo = line.Split(',');
 
-                lista.Add(arreglo[4] + ", Colombia");
-                count++;
+                lista.Add(arreglo[4] + ", Colombia"); //Se concatena el nombre del municipio con la cadena ", Colombia"
+                count++;                              //Para más precisión pueden concatenar el departamento.  
             }
 
         }
--- Gmaps/UserInterface/Interface.cs
+++ Gmaps/UserInterface/Interface.cs
@@ -14,13 +14,16 @@ namespace Gmaps
     {
 
         private DataManager dm;
-        private List<PointLatLng> puntos;
-        private List<PointLatLng> poligonos;
-        private List<PointLatLng> rutas;
 
-        GMapOverlay markers = new GMapOverlay("markers");
-        GMapOverlay polygons = new GMapOverlay("polygons");
-        GMapOverlay routes = new GMapOverlay("routes");
+        //Use tres listas para comodidad
+
+        private List<PointLatLng> puntos;       //Lista de puntos para los Marcadores
+        private List<PointLatLng> poligonos;    //Lista de puntos para los poligonos
+        private List<PointLatLng> rutas;        //Lista de puntos para las rutas
+
+        GMapOverlay markers = new GMapOverlay("markers");   //La "capa" donde iran los Marcadores
+        GMapOverlay polygons = new GMapOverlay("polygons"); //La "capa" donde iran los poligonos
+        GMapOverlay routes = new GMapOverlay("routes");     //La "capa" donde iran los rutas  
 
         public Interface()
         {
@@ -32,60 +35,64 @@ namespace Gmaps
             rutas = new List<PointLatLng>();
         }
 
-        private void gMap_Load(object sender, EventArgs e)
+        private void gMap_Load(object sender, EventArgs e) //Carga el GmapControl
         {
-            gmap.MapProvider = GoogleMapProvider.Instance;
+            gmap.MapProvider = GoogleMapProvider.Instance;  //Proveedor del servicio
             GMaps.Instance.Mode = AccessMode.ServerOnly;
 
-            gmap.Position = new PointLatLng(3.42158, -76.5205);
+            gmap.Position = new PointLatLng(3.42158, -76.5205); //No necesario, simplemente da una posicion para empezar
             
+
+            // Se agregan las "capas" al conjunto de capas del GmapControl
             gmap.Overlays.Add(markers);
             gmap.Overlays.Add(polygons);
             gmap.Overlays.Add(routes);
 
         }
         
-        private void setMarkers()
+        private void setMarkers()   //Coloca los marcadores en la capa
         {
-            foreach(PointLatLng p in puntos)
+            foreach(PointLatLng p in puntos) //P es un punto creado con latitud y longitud
             {
                 GMapMarker marker = new GMarkerGoogle(p, GMarkerGoogleType.red_dot);
-                markers.Markers.Add(marker);
+                markers.Markers.Add(marker); //Aqui se agrega el marcador a la capa
             }
 
         }
 
-        private void setPolygons()
+        private void setPolygons() //Extrapolar la explicación de los marcadores aqui...
         {
             GMapPolygon polygon = new GMapPolygon(poligonos, "Polygon");
 
-            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
-            polygon.Stroke = new Pen(Color.Red, 1);
+            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red)); //Lineas para modificar apariencia
+            polygon.Stroke = new Pen(Color.Red, 1);                       //No necesarias  
 
             polygons.Polygons.Add(polygon);
         }
 
-        private void setRoutes()
+        private void setRoutes() //Extrapolar la explicación de los marcadores aqui...
         {
             GMapRoute route = new GMapRoute(rutas, "Route");
             route.Stroke = new Pen(Color.Red, 3);
-            Console.WriteLine(route.Distance);
+            Console.WriteLine(route.Distance); //Metodo que retorna la distancia comprendida en la ruta (KM)
             routes.Routes.Add(route);
         }
 
-        private void municipios_Click(object sender, EventArgs e)
+        private void municipios_Click(object sender, EventArgs e)   //Mostrar municipios de Colombia
         {
-            List<string> lista = dm.getLista();
+            List<string> lista = dm.getLista(); //Trae los nombres de los municipios desde el Model
 
             foreach (string f in lista)
             {
-                GeoCoderStatusCode statusCode;
+                GeoCoderStatusCode statusCode;  
                 PointLatLng? pointLatLng1 = OpenStreet4UMapProvider.Instance.GetPoint(f, out statusCode);
+
+                //Las anteriores dos lineas proveen las funcionalidades para hacer la georeferenciación inversa
 
                 if (pointLatLng1 != null)
                 {
                     GMapMarker marker00 = new GMarkerGoogle(new PointLatLng(pointLatLng1.Value.Lat, pointLatLng1.Value.Lng), GMarkerGoogleType.blue_dot);
-                    marker00.ToolTipText = f + "\n" + pointLatLng1.Value.Lat + "\n" + pointLatLng1.Value.Lng;
+                    marker00.ToolTipText = f + "\n" + pointLatLng1.Value.Lat + "\n" + pointLatLng1.Value.Lng; // Esta linea es solo apariencia
                     markers.Markers.Add(marker00);
                     
                 }
@@ -94,7 +101,7 @@ namespace Gmaps
         }
 
 
-        private void deletePoints_click(object sender, EventArgs e)
+        private void deletePoints_click(object sender, EventArgs e) //Limpia todas las capas
         {
             puntos.Clear();
             markers.Clear();
@@ -102,10 +109,10 @@ namespace Gmaps
             routes.Clear();
         }
 
-        private void Add_Click(object sender, EventArgs e)
+        private void Add_Click(object sender, EventArgs e) //Añade PointsLatLng a la lista especificada en el comboBox
         {
 
-            double lat = double.Parse(lat_textBox.Text);
+            double lat = double.Parse(lat_textBox.Text); //Si te pc esta en español, usa comas con los decimales
             lat_textBox.Text = "";
             double lon = double.Parse(lon_textBox.Text);
             lon_textBox.Text = "";
@@ -120,7 +127,7 @@ namespace Gmaps
                 rutas.Add(p);
         }
 
-        private void Show_click(object sender, EventArgs e)
+        private void Show_click(object sender, EventArgs e) //Mostrar contenido de capas
         {
             if (comboBox.Text == "Marker")
             {
